5be757833c15546c55c440df120c32ff6d58260c
Ermin Hadzic <ehadzic4@etf.unsa.ba>
2023-02-18 11:07:55 +0100

    added an overloaded operator new

--- MemoryLeakDetector.cpp
+++ MemoryLeakDetector.cpp
@@ -1,20 +1,18 @@
-// MemoryLeakDetector.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <map>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+std::map<void*, std::pair<std::string, int>> alokacije;
+size_t ukupno_alocirano = 0, ukupno_dealocirano = 0;
+
+void* operator new(size_t vel, const char* file, int linije) {
+	void* pok = malloc(vel);
+	alokacije[pok] = std::make_pair(file, linije);
+	ukupno_alocirano += vel;
+	std::cout << "Alocirano " << vel << " bajta na adresi " << pok << " u fajlu " << file << " na liniji " << linije << std::endl;
+	return pok;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	return 0;
+}
