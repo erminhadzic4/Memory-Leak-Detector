11c042448eca10633f4971dc826b78b74d5383a5
Ermin Hadzic <ehadzic4@etf.unsa.ba>
2023-02-18 12:02:36 +0100

    added check(), clear(), check - checks for memory leaks, clear - clears the vector of all the memory leak info

--- MemoryLeakDetector.cpp
+++ MemoryLeakDetector.cpp
@@ -2,33 +2,58 @@
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
+#include <vector>
 
 bool write_to_file = false;
 #define WRITE_TO_FILE(enable) write_to_file = enable;
 #define DEBUG_NEW new(__FILE__, __LINE__)
 
-void* operator new(std::size_t size, const char* file, int line)
-{
-    void* ptr = std::malloc(size);
-    std::cout << "Allocating " << size << " bytes at " << ptr << " in " << file << " line " << line << std::endl;
-
-    if (write_to_file)
-    {
-        std::ofstream out("memory_leaks.txt", std::ios::app);
-        out << "Allocating " << size << " bytes at " << ptr << " in " << file << " line " << line << std::endl;
-    }
-
-    return ptr;
-}
-
-void operator delete(void* ptr) noexcept
-{
-    std::free(ptr);
-}
-
 class MemoryLeakDetector
 {
+private:
+    static std::vector<void*> allocated_pointers;
+
 public:
+    static void add(void* ptr)
+    {
+        allocated_pointers.push_back(ptr);
+    }
+
+    static void remove(void* ptr)
+    {
+        auto it = std::find(allocated_pointers.begin(), allocated_pointers.end(), ptr);
+        if (it != allocated_pointers.end())
+        {
+            allocated_pointers.erase(it);
+        }
+    }
+
+    static void clear()
+    {
+        allocated_pointers.clear();
+    }
+
+    static void check()
+    {
+        if (!allocated_pointers.empty())
+        {
+            std::cout << "Memory leaks detected!" << std::endl;
+            for (void* ptr : allocated_pointers)
+            {
+                std::cout << "Memory leak at " << ptr << std::endl;
+                if (write_to_file)
+                {
+                    std::ofstream out("memory_leaks.txt", std::ios::app);
+                    out << "Memory leak at " << ptr << std::endl;
+                }
+            }
+        }
+        else
+        {
+            std::cout << "No memory leaks detected." << std::endl;
+        }
+    }
+
     static void start()
     {
         std::cout << "Memory Leak Detection Started" << std::endl;
@@ -53,23 +78,50 @@ public:
             out << "-----------------------------" << std::endl;
             out << "Memory Leak Detection Ended" << std::endl;
         }
+
+        check();
+        clear();
     }
 };
+
+std::vector<void*> MemoryLeakDetector::allocated_pointers;
+
+void* operator new(std::size_t size, const char* file, int line)
+{
+    void* ptr = std::malloc(size);
+    std::cout << "Allocating " << size << " bytes at " << ptr << " in " << file << " line " << line << std::endl;
+
+    if (write_to_file)
+    {
+        std::ofstream out("memory_leaks.txt", std::ios::app);
+        out << "Allocating " << size << " bytes at " << ptr << " in " << file << " line " << line << std::endl;
+    }
+
+    MemoryLeakDetector::add(ptr);
+
+    return ptr;
+}
+
+#define new DEBUG_NEW
+
+void operator delete(void* ptr) noexcept
+{
+    MemoryLeakDetector::remove(ptr);
+    std::free(ptr);
+}
+
+
 
 int main()
 {
     MemoryLeakDetector::start();
 
-    // Enable writing to file
-    WRITE_TO_FILE(true);
+    //WRITE_TO_FILE(true);
 
-    int* p = DEBUG_NEW int;
-    double* q = DEBUG_NEW double[10];
+    int* ptr1 = new int;
+    int* ptr2 = new int[10];
 
-    // Disable writing to file
-    WRITE_TO_FILE(false);
+    //WRITE_TO_FILE(false);
 
     MemoryLeakDetector::end();
-    
-    return 0;
 }
