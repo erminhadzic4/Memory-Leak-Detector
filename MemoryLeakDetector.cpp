37b71e6ff4e932ceb50c12dd89522c703938aa36
Ermin Hadzic <ehadzic4@etf.unsa.ba>
2023-02-18 11:41:45 +0100

    added a new macro to output the info into a file, rewrote operator new and added the .txt file output to the class

--- MemoryLeakDetector.cpp
+++ MemoryLeakDetector.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
 
+bool write_to_file = false;
+#define WRITE_TO_FILE(enable) write_to_file = enable;
 #define DEBUG_NEW new(__FILE__, __LINE__)
 
 void* operator new(std::size_t size, const char* file, int line)
 {
     void* ptr = std::malloc(size);
     std::cout << "Allocating " << size << " bytes at " << ptr << " in " << file << " line " << line << std::endl;
+
+    if (write_to_file)
+    {
+        std::ofstream out("memory_leaks.txt", std::ios::app);
+        out << "Allocating " << size << " bytes at " << ptr << " in " << file << " line " << line << std::endl;
+    }
+
     return ptr;
 }
 
@@ -23,12 +33,26 @@ public:
     {
         std::cout << "Memory Leak Detection Started" << std::endl;
         std::cout << "-----------------------------" << std::endl;
+
+        if (write_to_file)
+        {
+            std::ofstream out("memory_leaks.txt");
+            out << "Memory Leak Detection Started" << std::endl;
+            out << "-----------------------------" << std::endl;
+        }
     }
 
     static void end()
     {
         std::cout << "-----------------------------" << std::endl;
         std::cout << "Memory Leak Detection Ended" << std::endl;
+
+        if (write_to_file)
+        {
+            std::ofstream out("memory_leaks.txt", std::ios::app);
+            out << "-----------------------------" << std::endl;
+            out << "Memory Leak Detection Ended" << std::endl;
+        }
     }
 };
 
@@ -36,8 +60,14 @@ int main()
 {
     MemoryLeakDetector::start();
 
+    // Enable writing to file
+    WRITE_TO_FILE(true);
+
     int* p = DEBUG_NEW int;
     double* q = DEBUG_NEW double[10];
+
+    // Disable writing to file
+    WRITE_TO_FILE(false);
 
     MemoryLeakDetector::end();
     
